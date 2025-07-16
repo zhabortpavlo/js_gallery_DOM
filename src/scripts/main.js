70934c42463f3e0653b29a2dbd382b060e609d24
Pavlo Zhabort <zhabortp@gmail.com>
2025-07-16 13:33:35 +0300

    dev: finished task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const thumbs = document.getElementById('thumbs');
+  const largeImg = document.getElementById('largeImg');
+
+  thumbs.addEventListener('click', function (event) {
+    event.preventDefault(); 
+
+    let link = event.target.closest('a'); 
+    if (!link || !thumbs.contains(link)) return; 
+
+    const largeImageSrc = link.getAttribute('href');
+    largeImg.src = largeImageSrc;
+  });
+});
